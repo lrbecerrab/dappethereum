011931d6746102a8f18f7e8cbabbedcfe34509f7
lrbecerrab <lrbecerrab@unal.edu.co>
2023-11-28 17:49:58 -0500

    Remove watch.usePolling from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: "0.0.0.0",
     strictPort: true,
     port: 5775,
