bf5297e83b543f2db7463f25424bb37aad2237ae
lrbecerrab <lrbecerrab@unal.edu.co>
2024-01-21 07:50:08 -0500

    Update contract addresses in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ import etAbi from "./EnergyTransact.json";
 import mgAbi from "./MicroGrid.json";
 export const energyTransactAbi = etAbi.abi;
 export const microgridAbi = mgAbi.abi;
-export const etContractAddress = "0x298a824e8fc5D1E301eE49DBc1C02fCa29C5CA43";
-export const mgContractAddress = "0xda164db46F0d900F65aC7f3bF2551b897e16A57d";
+export const etContractAddress = "0x8C5a348af3EfEcA81d4fdc76d68188Cb97136D1B";
+export const mgContractAddress = "0x3039F0C669296359A90aFcc54c2b82EFc5190417";
